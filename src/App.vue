d96970a3fa2a87e1a7eb3908bca5beddf9a6dda6
YuanZH <389937586@qq.com>
2019-02-20 14:19:52 +0800

    '123'

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- 显示的是当前路由地址所对应的内容 -->
     <router-view/>
   </div>
 </template>
 
 <script>
+// 当一个文件以.vue结尾时，将这个文件叫做一个单文件组件
 export default {
   name: 'App'
 }
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 在es6里键和值如果相同的话可以省略一部分的内容，router === router:router
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
@@ -13,3 +14,5 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+// 路由（router)：根据网址的不同返回不同的内容给用户
