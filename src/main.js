e234b0533f7ca9be039c0800b67e60c97ba6a332
YuanZH <389937586@qq.com>
2019-02-21 10:28:24 +0800

    swiper

--- src/main.js
+++ src/main.js
@@ -7,9 +7,12 @@ import fastClick from 'fastclick'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 // 在es6里键和值如果相同的话可以省略一部分的内容，router === router:router
 /* eslint-disable no-new */
