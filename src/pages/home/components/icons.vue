d1d0fa95ea19a4464b33a5235f4d81faced3172a
YuanZH <389937586@qq.com>
2019-02-21 12:07:37 +0800

    add icons;

---
+++ src/pages/home/components/icons.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="icons">
+        <swiper>
+            <swiper-slide v-for="(page, index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                    <img class="icon-img-content" :src="item.imgUrl">
+                    </div>
+                    <p class="icon-desc">{{item.desc}}</p>
+                </div>
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '必游'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/8c/47630407f70e8302.png',
+        desc: '影视基地'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+        desc: '游乐场'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+        desc: '文化古迹'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+        desc: '泡温泉'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+        desc: '城市观光'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+        desc: '动植物园'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8) // 向下取整的除法
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/varibles.styl';
+  @import '~styles/mixins.styl';
+  .icons >>> .swiper-container
+    width : 100%
+    height : 0
+    padding-bottom : 50% //宽高比为2
+  .icon
+    position : relative
+    overflow : hidden
+    width : 25%
+    height : 0
+    float : left
+    padding-bottom : 25%
+    .icon-img
+      position : absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing : border-box
+      padding: .1rem
+      .icon-img-content
+        height : 100%
+        display : block
+        margin : 0 auto
+    .icon-desc
+      position : absolute
+      left : 0
+      right : 0
+      bottom : 0
+      line-height : .44rem
+      height : .44rem
+      text-align : center
+      color : $darkTextColor
+      overflow : hidden
+      white-space : nowrap
+      text-overflow : ellipsis
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,17 +2,20 @@
   <div>
     <Home-header></Home-header>
     <Home-swiper></Home-swiper>
+    <Home-icons></Home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/header'
 import HomeSwiper from './components/swiper'
+import HomeIcons from './components/icons'
 export default {
   name: 'Home',
   components: {
     HomeHeader: HomeHeader,
-    HomeSwiper: HomeSwiper
+    HomeSwiper: HomeSwiper,
+    HomeIcons
   }
 }
 </script>
