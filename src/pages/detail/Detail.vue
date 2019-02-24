d5c795cc256683b91321d7a66e300a628bb46866
YuanZH <389937586@qq.com>
2019-02-24 12:43:50 +0800

    detail-head

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
   <detail-banner></detail-banner>
+  <detail-header></detail-header>
+  <div class="content"></div>
   </div>
 </template>
 
 <script>
 import detailBanner from './components/banner.vue'
+import detailHeader from './components/header.vue'
 export default {
   name: 'Detail',
   components: {
-    detailBanner
+    detailBanner,
+    detailHeader
   }
 }
 </script>
 
 <style lang='stylus' scoped>
-
+  .content
+    height 50rem
 </style>
---
+++ src/pages/detail/components/header.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <router-link class="header-abs" tag="div" to="/" v-show="showAbs">
+      <div class="iconfont header-abs-back">&#xe624;</div>
+    </router-link>
+    <!-- 渐隐渐现 -->
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link to="/">
+        <div class="iconfont header-fixed-back">&#xe624;</div>
+      </router-link>
+      景点详情</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 40) {
+        let opacity = top / 120
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
+  .header-abs
+    position absolute
+    left .2rem
+    top .2rem
+    width .6rem
+    height .6rem
+    line-height .6rem
+    text-align center
+    border-radius .4rem
+    background rgba(0, 0, 0, .8)
+    .header-abs-back
+      color #ffffff
+      font-size .4rem
+  .header-fixed
+    position : fixed
+    top 0
+    left 0
+    right 0
+    height: $headerHeight
+    line-height : $headerHeight
+    text-align : center
+    color : #ffffff
+    background : $bgColor
+    font-size : .32rem
+    .header-fixed-back
+      position : absolute
+      top : 0
+      left : 0
+      width : .64rem
+      text-align : center
+      font-size : .4rem
+      color : #ffffff
+</style>
